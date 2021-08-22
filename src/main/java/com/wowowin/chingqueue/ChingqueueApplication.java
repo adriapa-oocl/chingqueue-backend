5876ce4310743592edc60176ea0bfad4facc4e65
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-22 18:18:31 +0800

    initial commit

---
+++ src/main/java/com/wowowin/chingqueue/ChingqueueApplication.java
@@ -0,0 +1,13 @@
+package com.wowowin.chingqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChingqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChingqueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wowowin/chingqueue/ChingqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wowowin.chingqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChingqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
