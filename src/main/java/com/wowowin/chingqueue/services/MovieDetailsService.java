36ac6fc4c084b0209285de7457f12bd986d5b097
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-24 13:19:20 +0800

    [ST203][Juan Carlos] feat: Added MovieDetailsService CRUD

--- src/main/java/com/wowowin/chingqueue/services/MovieDetailsService.java
+++ src/main/java/com/wowowin/chingqueue/services/MovieDetailsService.java
@@ -1,7 +1,11 @@
 package com.wowowin.chingqueue.services;
 
+import com.wowowin.chingqueue.models.entities.MovieDetails;
 import com.wowowin.chingqueue.repositories.MovieDetailsRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieDetailsService {
@@ -10,6 +14,39 @@ public class MovieDetailsService {
 
     public MovieDetailsService(MovieDetailsRepository movieDetailsRepository) {
         this.movieDetailsRepository = movieDetailsRepository;
+    }
+
+    public List<MovieDetails> getAllMovieDetails() {
+        return movieDetailsRepository.findAll();
+    }
+
+    public MovieDetails addMovieDetails(MovieDetails MovieDetails) {
+        return movieDetailsRepository.save(MovieDetails);
+    }
+
+    public MovieDetails updateMovieDetails(Integer movie_Details_Id, MovieDetails movieDetailsInfo) {
+        MovieDetails updateMovieDetails = movieDetailsRepository.findById(movie_Details_Id)
+                .map(movieDetails -> updateMovieDetailsInfo(movieDetails, movieDetailsInfo))
+                .get();
+        return movieDetailsRepository.save(updateMovieDetails);
+    }
+
+    private MovieDetails updateMovieDetailsInfo(MovieDetails movieDetails, MovieDetails movieDetailsInfo) {
+        if (movieDetailsInfo.getMovie_Description() != null) {
+            movieDetails.setMovie_Description(movieDetailsInfo.getMovie_Description());
+        }
+
+        if (movieDetailsInfo.getMovie_Genre() != null) {
+            movieDetails.setMovie_Genre(movieDetailsInfo.getMovie_Genre());
+        }
+
+        return movieDetails;
+    }
+
+    public MovieDetails removeMovieDetails(Integer movie_Details_Id) {
+        Optional<MovieDetails> removeMovieDetails = movieDetailsRepository.findById(movie_Details_Id);
+        movieDetailsRepository.deleteById(movie_Details_Id);
+        return removeMovieDetails.orElse(null);
     }
 
 }
