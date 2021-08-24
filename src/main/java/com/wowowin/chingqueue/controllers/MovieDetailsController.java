def205d1ea923955d8b2326add20462f0a981f9b
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-24 13:29:59 +0800

    [ST203][Juan Carlos] feat: implemented MovieDetailsController CRUD

--- src/main/java/com/wowowin/chingqueue/controllers/MovieDetailsController.java
+++ src/main/java/com/wowowin/chingqueue/controllers/MovieDetailsController.java
@@ -1,11 +1,49 @@
 package com.wowowin.chingqueue.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.wowowin.chingqueue.mapper.MovieDetailsMapper;
+import com.wowowin.chingqueue.models.entities.MovieDetails;
+import com.wowowin.chingqueue.services.MovieDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieDetails")
 public class MovieDetailsController {
+
+    @Autowired
+    private MovieDetailsService movieDetailsService;
+
+    @Autowired
+    private MovieDetailsMapper movieDetailsMapper;
+
+    public MovieDetailsController(MovieDetailsService movieDetailsService, MovieDetailsMapper movieDetailsMapper) {
+        this.movieDetailsService = movieDetailsService;
+        this.movieDetailsMapper = movieDetailsMapper;
+    }
+
+    @GetMapping
+    public List<MovieDetails> getAllMovieDetails() {
+        return movieDetailsService.getAllMovieDetails();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MovieDetails addMovieDetails(@RequestBody MovieDetails movieDetails) {
+        return movieDetailsService.addMovieDetails(movieDetails);
+    }
+
+    @PutMapping(path = "/{movie_Details_Id}")
+    public MovieDetails updateMovieDetails(@PathVariable Integer movie_Details_Id, @RequestBody MovieDetails movieDetailsInfo) {
+        return movieDetailsService.updateMovieDetails(movie_Details_Id, movieDetailsInfo);
+    }
+
+    @DeleteMapping(path = "/{movie_Details_Id}")
+    public MovieDetails deleteMovieDetails(@PathVariable Integer movie_Details_Id) {
+        return movieDetailsService.removeMovieDetails(movie_Details_Id);
+    }
 
 
 }
