321d4dbc50eaa9719f0c85ba340912404efe2d96
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-24 13:08:19 +0800

    [ST203][Juan Carlos] feat: Added MovieDetailsController

---
+++ src/main/java/com/wowowin/chingqueue/controllers/MovieDetailsController.java
@@ -0,0 +1,11 @@
+package com.wowowin.chingqueue.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movieDetails")
+public class MovieDetailsController {
+
+
+}
