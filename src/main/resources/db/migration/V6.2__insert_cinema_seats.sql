a44f13de653bb9fa5241239fc2e56eaa30d9e48b
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-26 22:13:09 +0800

    [ST303][Juan Carlos Salvador] doc: change 1 to true

--- src/main/resources/db/migration/V6.2__insert_cinema_seats.sql
+++ src/main/resources/db/migration/V6.2__insert_cinema_seats.sql
@@ -1,387 +1,387 @@
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 1, 1);
+VALUES (1, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 1, 1);
+VALUES (2, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 1, 1);
+VALUES (3, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 1, 1);
+VALUES (4, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 1, 1);
+VALUES (5, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 1, 1);
+VALUES (6, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 1, 1);
+VALUES (7, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 1, 1);
+VALUES (8, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 1, 1);
+VALUES (9, 1, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 1, 1);
+VALUES (10, 1, true);
 
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 2, 1);
+VALUES (1, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 2, 1);
+VALUES (2, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 2, 1);
+VALUES (3, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 2, 1);
+VALUES (4, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 2, 1);
+VALUES (5, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 2, 1);
+VALUES (6, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 2, 1);
+VALUES (7, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 2, 1);
+VALUES (8, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 2, 1);
+VALUES (9, 2, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 2, 1);
+VALUES (10, 2, true);
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 3, 1);
+VALUES (1, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 3, 1);
+VALUES (2, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 3, 1);
+VALUES (3, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 3, 1);
+VALUES (4, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 3, 1);
+VALUES (5, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 3, 1);
+VALUES (6, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 3, 1);
+VALUES (7, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 3, 1);
+VALUES (8, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 3, 1);
+VALUES (9, 3, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 3, 1);
+VALUES (10, 3, true);
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 4, 1);
+VALUES (1, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 4, 1);
+VALUES (2, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 4, 1);
+VALUES (3, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 4, 1);
+VALUES (4, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 4, 1);
+VALUES (5, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 4, 1);
+VALUES (6, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 4, 1);
+VALUES (7, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 4, 1);
+VALUES (8, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 4, 1);
+VALUES (9, 4, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 4, 1);
+VALUES (10, 4, true);
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 5, 1);
+VALUES (1, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 5, 1);
+VALUES (2, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 5, 1);
+VALUES (3, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 5, 1);
+VALUES (4, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 5, 1);
+VALUES (5, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 5, 1);
+VALUES (6, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 5, 1);
+VALUES (7, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 5, 1);
+VALUES (8, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 5, 1);
+VALUES (9, 5, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 5, 1);
+VALUES (10, 5, true);
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 6, 1);
+VALUES (1, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 6, 1);
+VALUES (2, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 6, 1);
+VALUES (3, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 6, 1);
+VALUES (4, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 6, 1);
+VALUES (5, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 6, 1);
+VALUES (6, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 6, 1);
+VALUES (7, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 6, 1);
+VALUES (8, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 6, 1);
+VALUES (9, 6, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 6, 1);
+VALUES (10, 6, true);
 
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 7, 1);
+VALUES (1, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 7, 1);
+VALUES (2, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 7, 1);
+VALUES (3, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 7, 1);
+VALUES (4, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 7, 1);
+VALUES (5, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 7, 1);
+VALUES (6, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 7, 1);
+VALUES (7, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 7, 1);
+VALUES (8, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 7, 1);
+VALUES (9, 7, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 7, 1);
+VALUES (10, 7, true);
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 8, 1);
+VALUES (1, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 8, 1);
+VALUES (2, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 8, 1);
+VALUES (3, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 8, 1);
+VALUES (4, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 8, 1);
+VALUES (5, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 8, 1);
+VALUES (6, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 8, 1);
+VALUES (7, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 8, 1);
+VALUES (8, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 8, 1);
+VALUES (9, 8, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 8, 1);
+VALUES (10, 8, true);
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 9, 1);
+VALUES (1, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 9, 1);
+VALUES (2, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 9, 1);
+VALUES (3, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 9, 1);
+VALUES (4, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 9, 1);
+VALUES (5, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 9, 1);
+VALUES (6, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 9, 1);
+VALUES (7, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 9, 1);
+VALUES (8, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 9, 1);
+VALUES (9, 9, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 9, 1);
+VALUES (10, 9, true);
 
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 10, 1);
+VALUES (1, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 10, 1);
+VALUES (2, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 10, 1);
+VALUES (3, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 10, 1);
+VALUES (4, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 10, 1);
+VALUES (5, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 10, 1);
+VALUES (6, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 10, 1);
+VALUES (7, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 10, 1);
+VALUES (8, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 10, 1);
+VALUES (9, 10, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 10, 1);
+VALUES (10, 10, true);
 
 
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 11, 1);
+VALUES (1, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 11, 1);
+VALUES (2, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 11, 1);
+VALUES (3, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 11, 1);
+VALUES (4, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 11, 1);
+VALUES (5, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 11, 1);
+VALUES (6, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 11, 1);
+VALUES (7, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 11, 1);
+VALUES (8, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 11, 1);
+VALUES (9, 11, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 11, 1);
+VALUES (10, 11, true);
 
 
 
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (1, 12, 1);
+VALUES (1, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (2, 12, 1);
+VALUES (2, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (3, 12, 1);
+VALUES (3, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (4, 12, 1);
+VALUES (4, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (5, 12, 1);
+VALUES (5, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (6, 12, 1);
+VALUES (6, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (7, 12, 1);
+VALUES (7, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (8, 12, 1);
+VALUES (8, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (9, 12, 1);
+VALUES (9, 12, true);
 
 INSERT INTO CINEMA_SEAT (seat_Id, cinema_Id, availability)
-VALUES (10, 12, 1);
+VALUES (10, 12, true);
