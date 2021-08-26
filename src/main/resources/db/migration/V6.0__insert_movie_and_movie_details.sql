2d6cf1ca1da9d766aa50f88fac8284160e6e24d2
Juan Carlos Salvador <salvaju@oocl.com>
2021-08-26 21:53:34 +0800

    [ST303][Juan Carlos Salvador] Add Initial Data

---
+++ src/main/resources/db/migration/V6.0__insert_movie_and_movie_details.sql
@@ -0,0 +1,23 @@
+INSERT INTO MOVIE (movie_name, movie_img)
+VALUES ('Parasite', 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg');
+
+INSERT INTO MOVIE_DETAILS (movie_id, movie_description, movie_genre)
+VALUES (1, 'Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.', 'Thriller');
+
+INSERT INTO MOVIE (movie_name, movie_img)
+VALUES ('Man to Man', 'https://i.pinimg.com/564x/c2/4c/d9/c24cd920faacaa8dbaa2cbbafdf83ac9.jpg');
+
+INSERT INTO MOVIE_DETAILS (movie_id, movie_description, movie_genre)
+VALUES (2, 'A longtime celebrity Yeo Woon-gwang (Park Sung-woong) suddenly has a need for a bodyguard and hires Kim Seol-woo (Park Hae-jin), a handsome and mysterious man trained in special investigations who has many talents.', 'Action, Thriller, Melodrama, Comedy');
+
+INSERT INTO MOVIE (movie_name, movie_img)
+VALUES ('Always', 'https://i.pinimg.com/564x/b3/42/d6/b342d651598fde2d9cfa8276c0a68df9.jpg');
+
+INSERT INTO MOVIE_DETAILS (movie_id, movie_description, movie_genre)
+VALUES (3, 'A former boxer turned parking lot attendant finds hope in the love of the blind telemarketer who walks into his booth one day.', 'Drama');
+
+INSERT INTO MOVIE (movie_name, movie_img)
+VALUES ('Because I Love You', 'https://i.pinimg.com/564x/05/f7/ea/05f7ea4755741c023f9e75e3df4b8212.jpg');
+
+INSERT INTO MOVIE_DETAILS (movie_id, movie_description, movie_genre)
+VALUES (4, 'Jin Lee-Hyung (Cha Tae-Hyun) is a talented songwriter. He happens to connect people who are not good in love by taking over someones body.Those include Lee Hyun-Kyung (Seo Hyun-Jin) who wants to become a singer, but she suffers from stage fright.', 'Romantic-Comedy');
---
+++ src/main/resources/db/migration/V6.1__insert_cinema.sql
@@ -0,0 +1,45 @@
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('SM Cinema 1', 'Sta Rosa', 1, 'September 8 2021, 13:00PM');
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('Ronbinsons Cinema 1', 'Manila', 1, 'September 8 2021, 15:00PM');
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('Ayala Malls Cinema 1', 'Pasig', 1, 'September 8 2021, 17:00PM');
+
+
+
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('SM Cinema 1', 'Ortigas', 2, 'September 8 2021, 13:00PM');
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('Ronbinsons Cinema 1', 'Rizal', 2, 'September 8 2021, 15:00PM');
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('Ayala Malls Cinema 1', 'Calamba', 2, 'September 8 2021, 17:00PM');
+
+
+
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('SM Cinema 1', 'Cubao', 3, 'September 8 2021, 13:00PM');
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('Ronbinsons Cinema 1', 'Makati', 3, 'September 8 2021, 15:00PM');
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('Ayala Malls Cinema 1', 'Tagaytay', 3, 'September 8 2021, 17:00PM');
+
+
+
+
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('SM Cinema 1', 'Pasay', 4, 'September 8 2021, 13:00PM');
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('Ronbinsons Cinema 1', 'Alabang', 4, 'September 8 2021, 15:00PM');
+
+INSERT INTO CINEMA (cinema_company, cinema_location, movie_Id, cinema_timeSlot)
+VALUES ('Ayala Malls Cinema 1', 'Muntinlupa', 4, 'September 8 2021, 17:00PM');
